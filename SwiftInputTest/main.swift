e76460a3747f21c00f2184391e7d773da9c9bec0
Guillaume DIDIER <guillaume.didier@polytechnique.edu>
2015-01-28 20:46:30 +0100

    New test utility
    Used to check that the library works.

---
+++ SwiftInputTest/main.swift
@@ -0,0 +1,14 @@
+//
+//  main.swift
+//  SwiftInputTest
+//
+//  Created by Guillaume DIDIER on 28/01/2015.
+//
+//
+
+import Foundation
+
+
+let greeting = SIAskString(prompt: "What's the new greeting ? ")
+
+println(greeting)
